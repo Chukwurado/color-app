57a5b58587abd4b40add136e79601b6fb209bda8
Chukwurado98 <chukwurado.umeaka98@gmail.com>
2019-06-20 22:29:09 -0400

    Style ColorBox

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -6,7 +6,13 @@ export class ColorBox extends Component {
     const { background, name } = this.props;
     return (
       <div style={{ background }} className="ColorBox">
-        <span>{name}</span>
+        <div className="copy-container">
+          <div className="box-content">
+            <span>{name}</span>
+          </div>
+          <button className="copy-button">Copy</button>
+        </div>
+        <span className="see-more">More</span>
       </div>
     );
   }
