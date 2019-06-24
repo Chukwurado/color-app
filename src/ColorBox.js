f3bd1200438e8094a82e9bfaf7d1c4e348549c68
Chukwurado98 <chukwurado.umeaka98@gmail.com>
2019-06-24 14:24:01 -0400

    Refactor ColorBox Styles

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,8 +1,68 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import { withStyles } from "@material-ui/styles";
 import chroma from "chroma-js";
 import "./ColorBox.css";
+
+const styles = {
+  ColorBox: {
+    width: "20%",
+    height: props => (props.showingFullPalette ? "25%" : "50%"),
+    margin: "0 auto",
+    display: "inline-block",
+    position: "relative",
+    cursor: "pointer",
+    marginBottom: "-3.9px",
+    "&:hover button": {
+      opacity: 1
+    }
+  },
+  copyText: {
+    color: props =>
+      chroma(props.background).luminance() >= 0.5 ? "black" : "white"
+  },
+  colorName: {
+    color: props =>
+      chroma(props.background).luminance() > 0.1 ? "black" : "white"
+  },
+  seeMore: {
+    color: props =>
+      chroma(props.background).luminance() > 0.7 ? "rgba(0,0,0,0.5)" : "white",
+    background: "rgba(255, 255, 255, 0.3)",
+    position: "absolute",
+    border: "none",
+    right: "0",
+    bottom: "0",
+    width: "60px",
+    height: "30px",
+    textAlign: "center",
+    lineHeight: "30px",
+    textTransform: "uppercase"
+  },
+  copyButton: {
+    color: props =>
+      chroma(props.background).luminance() > 0.7 ? "rgba(0,0,0,0.5)" : "white",
+    width: "100px",
+    height: "30px",
+    position: "absolute",
+    display: "inline-block",
+    top: "50%",
+    left: "50%",
+    /* To make the button at the center */
+    marginLeft: "-50px",
+    marginTop: "-15px",
+    outline: "none",
+    background: "rgba(255, 255, 255, 0.3)",
+    fontSize: "1rem",
+    lineHeight: "30px",
+    textTransform: "uppercase",
+    border: "none",
+    textDecoration: "none",
+    textAlign: "center",
+    opacity: 0
+  }
+};
 
 export class ColorBox extends Component {
   constructor(props) {
@@ -16,38 +76,41 @@ export class ColorBox extends Component {
     //this.setState({ copied: true });
   };
   render() {
-    const { background, name, id, paletteId, showLink } = this.props;
+    const {
+      background,
+      name,
+      id,
+      paletteId,
+      showingFullPalette,
+      classes
+    } = this.props;
     const { copied } = this.state;
-    const isDarkColor = chroma(background).luminance() <= 0.1;
-    const isLightColor = chroma(background).luminance() >= 0.5;
+    // const isDarkColor = chroma(background).luminance() <= 0.1;
+    // const isLightColor = chroma(background).luminance() >= 0.5;
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
-        <div style={{ background }} className="ColorBox">
+        <div style={{ background }} className={classes.ColorBox}>
           <div
             className={`copy-overlay ${copied && "show"}`}
             style={{ background }}
           />
           <div className={`copy-msg ${copied && "show"}`}>
             <h1>Copied!</h1>
-            <p className={isLightColor && "dark-text"}>{background}</p>
+            <p className={classes.copyText}>{background}</p>
           </div>
           <div className="copy-container">
             <div className="box-content">
-              <span className={isDarkColor && "light-text"}>{name}</span>
+              <span className={classes.colorName}>{name}</span>
             </div>
-            <button className={`copy-button ${isLightColor && "dark-text"}`}>
-              Copy
-            </button>
+            <button className={classes.copyButton}>Copy</button>
           </div>
           {/* prevent other event from parent */}
-          {showLink && (
+          {showingFullPalette && (
             <Link
               to={`/palette/${paletteId}/${id}`}
               onClick={e => e.stopPropagation()}
             >
-              <span className={`see-more ${isLightColor && "dark-text"}`}>
-                MORE
-              </span>
+              <span className={classes.seeMore}>MORE</span>
             </Link>
           )}
         </div>
@@ -56,4 +119,4 @@ export class ColorBox extends Component {
   }
 }
 
-export default ColorBox;
+export default withStyles(styles)(ColorBox);
--- src/Palette.js
+++ src/Palette.js
@@ -27,7 +27,7 @@ export class Palette extends Component {
         key={color.id}
         id={color.id}
         paletteId={id}
-        showLink={true}
+        showingFullPalette={true}
       />
     ));
     return (
